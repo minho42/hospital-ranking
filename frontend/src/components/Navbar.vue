d75d6a3a4e5e870049e05cba8bd8fc556df9844d
Min ho Kim <minho42@gmail.com>
2020-12-22 00:59:05 +1100

    remove github repo, twitter links

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,35 +4,7 @@
       Australian hospitals ranked by Google reviews
     </h1>
     <!-- TODO Make update modified date automatically -->
-    <h2 class="text-gray-600">Collected 7/Oct/2020</h2>
-    <ul class="pb-5">
-      <li>
-        <div>
-          github:
-          <a
-            class="border-b-2 border-green-600"
-            href="https://github.com/minho42/hospital-ranking"
-            target="_blank"
-            rel="noopener noreferrer"
-            >backend</a
-          >,
-          <a
-            class="border-b-2 border-green-600"
-            href="https://github.com/minho42/hospital-ranking-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-            >frontend</a
-          >
-          <a
-            class="border-b-2 border-green-600 ml-4"
-            href="https://twitter.com/minhokim42"
-            target="_blank"
-            rel="noopener noreferrer"
-            >twitter</a
-          >
-        </div>
-      </li>
-    </ul>
+    <h2 class="text-gray-600 pb-4">Collected 7/Oct/2020</h2>
   </div>
 </template>
 
