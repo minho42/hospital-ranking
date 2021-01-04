94cc551ac9d0b481d7a85b33c34a2fe54e33aaf6
Min ho Kim <minho42@gmail.com>
2021-01-04 18:27:27 +1100

    format

--- frontend/src/components/HospitalList.vue
+++ frontend/src/components/HospitalList.vue
@@ -2,26 +2,45 @@
   <div class="flex flex-col items-center">
     <div>
       <div class="bg-gray-200 rounded-md p-2">
-        <label v-for="(state, index) in states" :key="index" class="inline-flex items-center">
-          <input type="checkbox" :value="state" class="form-checkbox w-4 h-4 align-middle" v-model="checkedStates" />
-          <span class="ml-1 mr-5">{{state}} ({{ getStateCount(state) }})</span>  
+        <label
+          v-for="(state, index) in states"
+          :key="index"
+          class="inline-flex items-center"
+        >
+          <input
+            type="checkbox"
+            :value="state"
+            class="form-checkbox w-4 h-4 align-middle"
+            v-model="checkedStates"
+          />
+          <span class="ml-1 mr-5"
+            >{{ state }} ({{ getStateCount(state) }})</span
+          >
         </label>
       </div>
     </div>
     <div>
       <div class="bg-gray-200 rounded-md p-2 my-2">
-        <label v-for="(sector, index) in sectors" :key="index" class="inline-flex items-center">
-          <input type="radio" :value="sector"  class="form-radio w-4 h-4 align-middle" v-model="selectedSector" />
-          <span class="ml-1 mr-5">{{sector}}</span>
+        <label
+          v-for="(sector, index) in sectors"
+          :key="index"
+          class="inline-flex items-center"
+        >
+          <input
+            type="radio"
+            :value="sector"
+            class="form-radio w-4 h-4 align-middle"
+            v-model="selectedSector"
+          />
+          <span class="ml-1 mr-5">{{ sector }}</span>
         </label>
-
       </div>
     </div>
-    
+
     <div v-if="isLoading" class="text-xl font-bold py-3">
       Loading...
     </div>
-        
+
     <div v-else class="text-2xl font-bold py-3">
       {{ filteredRankingData.length }} hospitals
       <span v-show="getAverateStar">({{ getAverateStar.toFixed(2) }})</span>
@@ -52,9 +71,9 @@ export default {
   name: "HospitalList",
   data() {
     return {
-      states:["VIC", "NSW", "QLD", "TAS", "WA", "SA", "ACT", "NT"],
+      states: ["VIC", "NSW", "QLD", "TAS", "WA", "SA", "ACT", "NT"],
       checkedStates: ["VIC", "NSW", "QLD", "TAS", "WA", "SA", "ACT", "NT"],
-      sectors: ["ALL", "PUBLIC", "PRIVATE"], 
+      sectors: ["ALL", "PUBLIC", "PRIVATE"],
       selectedSector: "ALL",
     };
   },
@@ -67,53 +86,52 @@ export default {
   },
   methods: {
     getStateCount(state) {
-      let count  = 0;
-      this.rankingData.filter((item)=>{
+      let count = 0;
+      this.rankingData.filter((item) => {
         if (item.state === state) {
           count++;
         }
-      })
-      return count
-    }
+      });
+      return count;
+    },
   },
   computed: {
-    getAverateStar: function () {
+    getAverateStar: function() {
       return this.getStarsXreviews / this.getTotalReviewCount;
     },
-    getStarsXreviews: function () {
+    getStarsXreviews: function() {
       let total = 0;
       [...this.filteredRankingData].map((item) => {
         total += parseFloat(item.stars) * parseFloat(item.reviews);
       });
       return parseInt(total);
     },
-    getTotalReviewCount: function () {
+    getTotalReviewCount: function() {
       let total = 0;
       [...this.filteredRankingData].map((item) => {
         total += parseInt(item.reviews);
       });
       return total;
     },
-    sortedRankingData: function () {
+    sortedRankingData: function() {
       return [...this.rankingData].sort((a, b) => {
         return parseFloat(b.ranking) - parseFloat(a.ranking);
       });
     },
-    filteredRankingData: function () {
+    filteredRankingData: function() {
       return this.sortedRankingData
-      .filter((item) => {
-        return this.checkedStates.includes(item.state);
-      })
-      .filter((item)=>{
-        if (this.selectedSector === "ALL") {
-          return true
-        }
-        return this.selectedSector===item.sector
-      })
+        .filter((item) => {
+          return this.checkedStates.includes(item.state);
+        })
+        .filter((item) => {
+          if (this.selectedSector === "ALL") {
+            return true;
+          }
+          return this.selectedSector === item.sector;
+        });
     },
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
