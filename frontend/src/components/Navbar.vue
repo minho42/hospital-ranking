1a1cbdcb5b5f718841f50e68d56609c6f0a44d8b
Min ho Kim <minho42@gmail.com>
2021-01-04 18:27:17 +1100

    mt-3

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center mt-3">
     <h1 class="text-2xl font-bold text-center capitalize">
       Australian hospitals ranked by Google reviews
     </h1>
